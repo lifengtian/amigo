c36b9ca782c05d549812b3b30a8ac97de1b41690
Lifeng Tian <lifeng4209@gmail.com>
2013-04-14 21:57:01 -0400

    add mychan

--- src/mychan/mychan.go
+++ src/mychan/mychan.go
@@ -1,26 +1,48 @@
 //Play with channel and goroutine
+//42 us for 2 go
+//33 us for 4 go
+//Need to figure out how to test with different number of channels
 package main
 
 import "fmt"
 import "time"
+import "os"
+import "math/rand"
+import "runtime"
 
-func sum(v []int, c chan int) {
-	sum := 0
+// sum calculates the sum of all members of the slice
+func sum(v []int, c chan int64) {
+	var sum int64 = 0
 	for _, value := range v {
-		sum += value
+		sum += int64(value)
 	}
+
+	// instead of return the value, we send it to the channel
 	c <- sum
 }
 
 func main() {
-	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	t := time.Now()
-	c := make(chan int, 4)
-	go sum(values[:len(values)/4], c)
-	go sum(values[len(values)/4:len(values)/2], c)
-	go sum(values[len(values)/2:len(values)*3/4], c)
-	go sum(values[len(values)*3/4:], c)
+	NCPU := runtime.NumCPU()
+	N := NCPU / 2
+	runtime.GOMAXPROCS(N)
+	fmt.Printf("NCPU : %d N : %d\n", NCPU, N)
 
-	sum1, sum2, sum3, sum4 := <-c, <-c, <-c, <-c
-	fmt.Printf("Result in %v : %d + %d + %d + %d = %d\n", time.Since(t), sum1, sum2, sum3, sum4, sum1+sum2+sum3+sum4)
+	values := rand.Perm(100000000)
+	t := time.Now()
+	c := make(chan int64, N)
+
+	for i := 0; i < N; i++ {
+		go sum(values[len(values)*i/N:len(values)*(i+1)/N], c)
+	}
+
+	var mysum int64 = 0
+
+	// Drain the channel
+	for i := 0; i < N; i++ {
+		s := <-c
+		mysum += s
+		fmt.Println("go ", i, s)
+	}
+
+	fmt.Printf("Result in %v %v = %d\n", os.Args[0], time.Since(t), mysum)
 }
