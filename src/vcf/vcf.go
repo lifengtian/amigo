7c765a18359141c2fbb1ee0da17826600258e4cd
Lifeng Tian <lifeng4209@gmail.com>
2013-05-10 15:27:25 -0400

    add vcf.go

--- src/vcf/vcf.go
+++ src/vcf/vcf.go
@@ -1,45 +1,53 @@
 //package vcf provides an interface to VCF files
+//bufio has a maxTokenSize 64 * 1024 which turns out to be too small for some INDEL records
+//I changed it to 640 * 1024 in scanner.go
+//TODO: use Reader instead
+// LFT May 2013
 package main
 
-
 import (
-	"fmt" 
+	"bufio"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"regexp"
-	"bufio"
-	"log"
-	"io"
 	"strings"
 	"time"
-	"strconv"
+
+//	"strconv"
 )
 
-
-
-func main(){
+func main() {
 	var file io.Reader
 	var err error
-	s := regexp.MustCompile(`AF=([\.\d]+)`)
+	af := regexp.MustCompile(`AF=([\.\de\-,]+)`)
+	an := regexp.MustCompile(`AN=([\.\d]+)`)
+	ac := regexp.MustCompile(`AC=([\.\d,]+)`)
+	gene := regexp.MustCompile(`Gene=([^;]+)`)
 	var l int // length
 
 	t0 := time.Now()
-	if l = len(os.Args); l> 1 {
+	if l = len(os.Args); l > 1 {
 		file, err = os.Open(os.Args[1])
-		if err != nil { log.Fatal(err)}
-		fmt.Fprintf(os.Stdin, "os.Args len:%d", l )
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Fprintf(os.Stdin, "os.Args len:%d", l )
 	} else {
 		file = os.Stdin
 	}
 
 	scanner := bufio.NewScanner(file)
-	var m string
 	var text string
 	var fields []string
 	var n_fields int
-	var line_no int 
+	var line_no int
+	var m_af, m_an, m_ac, m_gene string
 
 	for scanner.Scan() {
 		text = scanner.Text()
+		//fmt.Printf("text:\n%s\n", text)
 		line_no++
 		// retrieve number of samples
 		// should it be that number of samples be provided in the header???
@@ -50,18 +58,32 @@ func main(){
 			n_fields = len(fields)
 			n_samples := n_fields - 9
 			fmt.Println("Number of samples: ", n_samples)
-		} else if ! strings.HasPrefix(text,"##"){
+			fmt.Printf("CHROM\tPOS\tID\tREF\tALT\tFILTER\tAltAlleleFreq\tTotalNumberofAllAlleles\tAltAlleleNumber\tGene\n")
+		} else if !strings.HasPrefix(text, "##") {
 			fields = strings.Fields(text)
-			if n_fields != len(fields) {log.Fatal("n_fields != len(fields) at line ", line_no)}
-			m = s.FindString( text )
-			if l = len(m); l>0 {
-				if i,_ := strconv.ParseFloat(strings.Trim(m,"AF="), 64); i < 0.1 {
-					fmt.Println(line_no, i, m)
-				}
+			if n_fields != len(fields) {
+				fmt.Println("error", text)
+				log.Fatal("n_fields != len(fields) at line ", line_no)
+			}
+
+			m_af = af.FindString(text)
+			m_an = an.FindString(text)
+			m_ac = ac.FindString(text)
+			m_gene = gene.FindString(text)
+			if l = len(m_af); l > 0 {
+				//if i,_ := strconv.ParseFloat(strings.Trim(m,"AF="), 64); i < 0.1 {
+				fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", fields[0], fields[1], fields[2], fields[3], fields[4], fields[6], m_af, m_an, m_ac, m_gene)
+				//}
 			}
 		}
+		//if a := line_no % 10 ; a == 0  {
+		//	fmt.Fprintf(os.Stderr,"%d\n",line_no)
+		//}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
 	t1 := time.Now()
-	fmt.Printf("Total run time: %v\n", t1.Sub(t0) )
+	fmt.Fprintf(os.Stderr, "Total run time: %v\n", t1.Sub(t0))
 }
